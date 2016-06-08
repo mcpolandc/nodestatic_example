0309c3cec55b5f5355b8d5eae5227e49a7cade90
cary.mcpoland <cary.mcpoland@biznetsolutions.com>
2016-06-08 10:46:44 +0100

    Added some error handling

--- nodestatic.js
+++ nodestatic.js
@@ -1,16 +1,37 @@
 
 //Import node-static and http libraries from node
-var nstatic = require('node-static'), http = require('http');
+var nstatic = require('node-static'), http = require('http'), util = require('util');
 
-var folder = './public', port = 8080;
+var folder = './public', port = 8080, version = "0.2";
 
 var fileServer = new(nstatic.Server)(folder);
 
-console.log('serving at %s , version 0.1', folder);
+console.log('serving at %s , version %s', folder, version);
 
 http.createServer(function(req, res) {
     req.addListener('end', function() {
-        fileServer.serve(req, res);
+        fileServer.serve(req, res, function(err, result) {
+            
+            if(err) {
+               
+                //Log attempted url and error message
+                console.error("URL: %s - ERROR: %s",req.url, err.message);
+                
+                //Handle 404 and 500 errors by serving the the appropriate file
+                if (err.status === 404 || err.status === 500) {
+                    fileServer.serveFile(util.format('%d.html', err.status), err.status, {}, req, res);
+                }
+                else {
+                    //Handle any other errors by just writing errors to the header of response
+                    res.writeHead(err.status, err.headers);
+                    res.end();
+                }
+            } else {
+                //Log method used and url accessed upon a successul request
+                console.log("%s : %s", req.method, req.url);
+            }
+            
+        });
     }).resume();
 }).listen(port);
 
